322bdded6b50f01b9969750a77dfdc84f3b8662f
Liwei Xu <liwei.xu@etu.emse.fr>
2022-01-23 02:57:02 +0100

    Update

---
+++ chap6/6.32.cpp
@@ -0,0 +1,13 @@
+#include "head.h"
+
+int &get(int *arry, int index) {return arry[index];}
+
+int main(){
+    int ia[10];
+    for (int i = 0; i != 10; ++i)
+        get(ia, i) = 1;
+    
+    for (auto a : ia){
+        cout << a << endl;
+    }
+}
